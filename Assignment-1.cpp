6101ad9152ffdef067c731d7cf874ce9f5e2b4ff
Felipe Chung <fchung.dev@gmail.com>
2021-03-18 12:35:41 -0500

    finish basic assignment

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -8,11 +8,10 @@
  void sortInefficient(int [], int );
  
  int main() {
- 	int SIZE=10000;
+ 	int SIZE=100000;
 	clock_t   start, end;  // available when ctime is included 
 	double    duration;
-	int dataBest[SIZE], dataAvg[SIZE], dataWorst[SIZE], dataBestIn[SIZE], dataAvgIn[SIZE], dataWorstIn[SIZE];
-	
+	int dataBest[SIZE], dataAvg[SIZE], dataWorst[SIZE];	
   
   ifstream myfile;
   
@@ -51,40 +50,76 @@
   }
   else cout <<"Unable to open file";
   
-//  start = clock() ; //Start timing
-//  sortInefficient(dataBestIn, SIZE);// Call sortefficientfunction 
-//  end = clock() ; //End timing
-//  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
-//  cout<<"\n\nInefficient best case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
-//  
-//    start = clock() ; //Start timing
-//  sortInefficient(dataAvgIn, SIZE);// Call sortefficientfunction 
-//  end = clock() ; //End timing
-//  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
-//  cout<<"\n\nInefficient average case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
-//  
-//      start = clock() ; //Start timing
-//  sortInefficient(dataWorstIn, SIZE);// Call sortefficientfunction 
-//  end = clock() ; //End timing
-//  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
-//  cout<<"\n\nInefficient worst case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
+  start = clock() ; //Start timing
+  sortInefficient(dataBest, SIZE);// Call sortInefficient function 
+  end = clock() ; //End timing
+  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
+  cout<<"\n\nInefficient best case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
+  
+  //Reset dataBest array
+  myfile.open("bestCase.txt"); //Open files 
+  
+  if (myfile.is_open())  {        
+    
+	for(int i=1; i<=SIZE; i++){ 
+		myfile>> dataBest[i] ; 	
+	}
+    myfile.close();
+  }
+  else cout << "Unable to open file"; 
+  
+    start = clock() ; //Start timing
+  sortInefficient(dataAvg, SIZE);// Call sortInefficient function 
+  end = clock() ; //End timing
+  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
+  cout<<"\n\nInefficient average case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
+  
+  //Reset dataAvg array
+  myfile.open("averageCase.txt");  
+  if(myfile.is_open()) {
+  	
+  	for(int i=1; i<=SIZE; i++){
+  		myfile>> dataAvg[i];
+  		
+	  }
+	  myfile.close();
+  }
+  else cout <<"Unable to open file";
+  
+      start = clock() ; 
+  sortInefficient(dataWorst, SIZE);
+  end = clock() ; 
+  duration=((double)(end - start))/CLOCKS_PER_SEC; 
+  cout<<"\n\nInefficient worst case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
+  
+  //Reset dataWorst array
+      myfile.open("worstCase.txt");  
+  if(myfile.is_open()) {
+  	
+  	for(int i=1; i<=SIZE; i++){
+  		myfile>> dataWorst[i];
+
+	  }
+	  myfile.close();
+  }
+  else cout <<"Unable to open file";
   
   start = clock() ; //Start timing
-  sortEfficient(dataBest, SIZE);// Call sortefficientfunction 
+  sortEfficient(dataBest, SIZE);// Call sortEfficient function 
   end = clock() ; //End timing
   duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
   cout<<"\n\nEfficient best case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
   
-  start = clock() ; //Start timing
-  sortEfficient(dataAvg, SIZE);
-  end = clock() ; //End timing
-  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
+  start = clock() ;
+  sortEfficient(dataAvg, SIZE); 
+  end = clock() ; 
+  duration=((double)(end - start))/CLOCKS_PER_SEC; 
   cout<<"\n\nEfficient average case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
   
-  start = clock() ; //Start timing
+  start = clock() ; 
   sortEfficient(dataWorst, SIZE);
-  end = clock() ; //End timing
-  duration=((double)(end - start))/CLOCKS_PER_SEC; //Compute seconds
+  end = clock() ; 
+  duration=((double)(end - start))/CLOCKS_PER_SEC; 
   cout<<"\n\nEfficient worst case took "<<setprecision(3)<<fixed<<duration<<" seconds!\n";
 	
 	
